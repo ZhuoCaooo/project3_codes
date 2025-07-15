a830f7274aeefd3717b0eddfcf9a9757b06235bc
Zhuo Cao <87007013+ZhuoCaooo@users.noreply.github.com>
2025-07-15 21:19:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from process_data import run
 import pickle
 import os
 
-if not os.path.exists("output_8sbefore_2safter"):
-    os.makedirs("output_8sbefore_2safter")
+if not os.path.exists("output_4sbefore_2safter"):
+    os.makedirs("output_4sbefore_2safter")
 
 total_change = 0
 for i in range(1, 61):
@@ -12,7 +12,7 @@ for i in range(1, 61):
     total_change += change_num
     print("total changes:", total_change)
 
-    filename = "output_8sbefore_2safter/result" + number + ".pickle"
+    filename = "output_4sbefore_2safter/result" + number + ".pickle"
     f = open(filename, 'wb')
     pickle.dump(result, f)
     print("Successfully write to:", filename)
